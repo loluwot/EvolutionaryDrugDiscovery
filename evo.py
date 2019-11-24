8fe97e158d5bb0fcbd06df5e6391e020c9219df6
Andy C <fontisawesome@gmail.com>
2019-11-23 20:38:11 -0500

    Added all possible mutations of molecule

--- evo.py
+++ evo.py
@@ -1,4 +1,3 @@
-
 from rdkit import Chem
 from rdkit.Chem import AllChem
 from rdkit.Chem import Draw
@@ -16,6 +15,17 @@ atom_dict = {
 	'F':[1],
 	'I':[1],
 	'P':[1,2]
+}
+atom_valences = {
+	'C':[4],
+	'N':[3],
+	'O':[2],
+	'S':[2,6],
+	'Cl':[1],
+	'Br':[1],
+	'F':[1],
+	'I':[1],
+	'P':[3,5]
 }
 # def mutate(mol, generation, n):
 	# for _ in range(n):
@@ -46,7 +56,7 @@ def addAtom(mol):
 	for atom in newMol.GetAtoms():
 		print(str(atom.GetImplicitValence()) + " " + atom.GetSymbol())
 	newMol = Chem.AddHs(newMol)
-	#AllChem.Compute2DCoords(newMol)
+	AllChem.Compute2DCoords(newMol)
 	#Draw.MolToFile(newMol,'test.png')
 	AllChem.EmbedMolecule(newMol)
 	print(newMol.GetNumAtoms())
@@ -70,14 +80,67 @@ def addBond(mol):
 			editable.RemoveBond(id1, id2)
 		editable.AddBond(id1, id2, bond_type[bondType-1])
 		newMol = editable.GetMol()
+		newMol.UpdatePropertyCache()
+		newMol = Chem.AddHs(newMol)
+		AllChem.Compute2DCoords(newMol)
+		AllChem.EmbedMolecule(newMol)
 		Draw.MolToFile(newMol, 'test.png')
-# def removeAtom(mol):
-	
-# def removeBond(mol):
-
-init_mol = Chem.MolFromSmiles("CC")
+def removeAtom(mol):
+	atoms = mol.GetAtoms()
+	r_atom = atoms[randint(0, len(atoms)-1)]
+	editable = Chem.rdchem.EditableMol(mol)
+	editable.RemoveAtom(r_atom.GetIdx())
+	newMol = editable.GetMol()
+	newMol.UpdatePropertyCache()
+	newMol = Chem.AddHs(newMol)
+	AllChem.Compute2DCoords(newMol)
+	frags = Chem.rdmolops.GetMolFrags(newMol,sanitizeFrags=True, asMols=True)
+	largest_frag = frags[0]
+	for mol_frag in frags:
+		if (mol_frag.GetNumAtoms() >= largest_frag.GetNumAtoms()):
+			largest_frag = mol_frag
+	largest_frag.UpdatePropertyCache()
+	AllChem.Compute2DCoords(largest_frag)		
+	AllChem.EmbedMolecule(largest_frag)
+	Draw.MolToFile(largest_frag, 'test.png')
+def removeBond(mol):
+	bonds = mol.GetBonds()
+	r_bond = bonds[randint(0, len(bonds)-1)]
+	editable = Chem.rdchem.EditableMol(mol)
+	editable.RemoveBond(r_bond.GetBeginAtomIdx(), r_bond.GetEndAtomIdx())
+	newMol = editable.GetMol()
+	newMol.UpdatePropertyCache()
+	newMol = Chem.AddHs(newMol)
+	AllChem.Compute2DCoords(newMol)
+	frags = Chem.rdmolops.GetMolFrags(newMol,sanitizeFrags=True, asMols=True)
+	largest_frag = frags[0]
+	for mol_frag in frags:
+		if (mol_frag.GetNumAtoms() >= largest_frag.GetNumAtoms()):
+			largest_frag = mol_frag
+	largest_frag.UpdatePropertyCache()
+	AllChem.Compute2DCoords(largest_frag)		
+	AllChem.EmbedMolecule(largest_frag)
+	Draw.MolToFile(largest_frag, 'test.png')
+def replaceAtom(mol):
+	atoms = mol.GetAtoms()
+	r_atom = atoms[randint(0, len(atoms)-1)]
+	possible = []
+	for s in possible_atoms:
+		if (r_atom.GetTotalValence() in atom_valences[s]):
+			possible.append(s)
+	print(possible)
+	a = Chem.rdchem.Atom(possible[randint(0, len(possible)-1)])
+	editable = Chem.rdchem.EditableMol(mol)
+	editable.ReplaceAtom(r_atom.GetIdx(), a)
+	newMol = editable.GetMol()
+	newMol.UpdatePropertyCache()
+	newMol = Chem.AddHs(newMol)
+	AllChem.Compute2DCoords(newMol)
+	AllChem.EmbedMolecule(newMol)
+	Draw.MolToFile(newMol, 'test.png')
+init_mol = Chem.MolFromSmiles("COCOCCOC")
 init_generation = [init_mol]
 num_generations = 2;
 final_generation_size = 100
 #addAtom(init_mol)
-addBond(init_mol)
+replaceAtom(init_mol)
